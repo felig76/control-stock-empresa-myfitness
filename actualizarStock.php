d7a1b54f2aa43f14b6b882ab5b2a23a0681331f4
felig76 <blakyn19@gmail.com>
2024-10-22 11:15:53 -0300

    Corregir php de actualizacion de stock

--- actualizarStock.php
+++ actualizarStock.php
@@ -3,7 +3,6 @@ header('Content-Type: application/json');
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
 error_log("Inicio de actualizarStock.php");
 
 $conn = new mysqli("localhost", "root", "", "myfitness_stock");
@@ -16,9 +15,9 @@ $data = json_decode(file_get_contents('php://input'), true);
 
 foreach ($data as $producto) {
     $nombre = $producto['nombre_producto'];
-    $cantidad = $producto['cantidad_producto'];
+    $cantidad = $producto['cantidad'];
     // Actualizar el stock en la base de datos
-    $sql = "UPDATE stock SET cantidad_producto = cantidad_producto - $cantidad WHERE nombre_producto = '$nombre'";
+    $sql = "UPDATE stock SET cantidad_producto = cantidad_producto + $cantidad WHERE nombre_producto = '$nombre'";
     if ($conn->query($sql) !== TRUE) {
         echo "Error actualizando el stock: " . $conn->error;
     }
